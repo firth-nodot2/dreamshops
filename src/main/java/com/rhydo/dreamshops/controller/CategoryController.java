714d7c8b449f9ce3f924ba9df769cdabbc49cbd6
firth-nodot2 <firthnodot2@gmail.com>
2025-06-06 22:27:32 +0800

    Commit 6.6

--- src/main/java/com/rhydo/dreamshops/controller/CategoryController.java
+++ src/main/java/com/rhydo/dreamshops/controller/CategoryController.java
@@ -1,4 +1,12 @@
 package com.rhydo.dreamshops.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+
 }
